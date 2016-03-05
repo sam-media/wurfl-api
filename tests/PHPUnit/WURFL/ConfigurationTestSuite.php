564a42f9353d53b7a7c08c94bd16cad613f0d79c
Thomas Müller <mimmi20@live.de>
2016-03-05 17:15:30 +0100

    remove test

--- tests/PHPUnit/WURFL/ConfigurationTestSuite.php
+++ tests/PHPUnit/WURFL/ConfigurationTestSuite.php
@@ -3,7 +3,6 @@
  * test case
  */
 require_once 'Configuration/ArrayConfigTest.php';
-require_once 'Configuration/XmlConfigTest.php';
 require_once 'Configuration/InMemoryConfigTest.php';
 
 /**
@@ -17,7 +16,6 @@ class WURFL_ConfigurationTestSuite extends PHPUnit_Framework_TestSuite
     public function __construct()
     {
         $this->setName('ConfigurationTestSuite');
-        $this->addTestSuite('WURFL_Configuration_XmlConfigTest');
         $this->addTestSuite('WURFL_Configuration_ArrayConfigTest');
         $this->addTestSuite('WURFL_Configuration_InMemoryConfigTest');
     }
